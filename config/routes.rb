58f978806cf5cffd8e56088a48bb0bdba6e74503
Amy Leigh Petrie <apetrie9849@gmail.com>
2019-02-26 15:44:22 -0700

    Add search route

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       get "/daily_pet", to: "pets#show", :id => "random"
+      get "/search", to: "search#new"
     end
   end
 
