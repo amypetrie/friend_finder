ea679425eb856da409edb41021d1b460b097fde5
Amy Leigh Petrie <apetrie9849@gmail.com>
2019-02-25 18:20:52 -0700

    Write test to search pets by location via pet.find endpoint

--- spec/services/petfinder_api_service_spec.rb
+++ spec/services/petfinder_api_service_spec.rb
@@ -1,6 +1,7 @@
 require "rails_helper"
 
 describe "PetFinder API Service" do
+
   it "returns a random pet ID" do
     VCR.use_cassette("petfinder_get_random_pet") do
       service = PetFinderService.new
@@ -20,7 +21,7 @@ describe "PetFinder API Service" do
       service = PetFinderService.new
 
       results = service.get_pet_by_id(44046167)
-      
+
       expect(results).to be_a Hash
       expect(results).to have_key :petfinder
       expect(results[:petfinder]).to have_key :pet
@@ -35,8 +36,22 @@ describe "PetFinder API Service" do
       expect(results[:petfinder][:pet]).to have_key :contact
     end
   end
-  #
-  # xit "returns a collection of pets based on search params" do
-  #
-  # end
+
+  it "returns a collection of pets based on search params" do
+    VCR.use_cassette("petfinder_search_pets") do
+      service = PetFinderService.new
+
+      filter = {"location": "denver,co"}
+      results = service.search(filter)
+
+      expect(results).to be_a Hash
+      expect(results).to have_key :petfinder
+      expect(results[:petfinder]).to have_key :lastOffset
+      expect(results[:petfinder]).to have_key :pets
+      expect(results[:petfinder][:pets]).to have_key :pet
+      expect(results[:petfinder][:pets][:pet]).to be_an Array
+      expect(results[:petfinder][:pets][:pet].length).to eq 25
+      expect(results[:petfinder][:pets][:pet].first).to have_key :name
+    end
+  end
 end
