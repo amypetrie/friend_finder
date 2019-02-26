ac60b71c4a95cd8fed73059c134d29271f86c353
Amy Leigh Petrie <apetrie9849@gmail.com>
2019-02-25 17:30:35 -0700

    Create test for pet.find by ID endpoint call

--- spec/services/petfinder_api_service_spec.rb
+++ spec/services/petfinder_api_service_spec.rb
@@ -5,7 +5,7 @@ describe "PetFinder API Service" do
     VCR.use_cassette("petfinder_get_random_pet") do
       service = PetFinderService.new
 
-      results = service.daily_pet
+      results = service.get_random_pet
 
       expect(results).to be_a Hash
       expect(results).to have_key :petfinder
@@ -14,9 +14,27 @@ describe "PetFinder API Service" do
       expect(results[:petfinder][:petIds][:id]).to have_key :$t
     end
   end
-  # xit "returns a pet's information by ID" do
-  #
-  # end
+
+  it "returns a pet's information by ID" do
+    VCR.use_cassette("petfinder_get_pet_by_id") do
+      service = PetFinderService.new
+
+      results = service.get_pet_by_id(44046167)
+      
+      expect(results).to be_a Hash
+      expect(results).to have_key :petfinder
+      expect(results[:petfinder]).to have_key :pet
+      expect(results[:petfinder][:pet]).to have_key :age
+      expect(results[:petfinder][:pet]).to have_key :size
+      expect(results[:petfinder][:pet]).to have_key :media
+      expect(results[:petfinder][:pet]).to have_key :breeds
+      expect(results[:petfinder][:pet]).to have_key :sex
+      expect(results[:petfinder][:pet]).to have_key :description
+      expect(results[:petfinder][:pet]).to have_key :animal
+      expect(results[:petfinder][:pet]).to have_key :status
+      expect(results[:petfinder][:pet]).to have_key :contact
+    end
+  end
   #
   # xit "returns a collection of pets based on search params" do
   #
